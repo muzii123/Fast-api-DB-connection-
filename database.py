62ad836c3c8c95ddf58cfdb6688d16b39a4a21db
muzii123 <mmuzamiljameelahmad@gmail.com>
2026-06-04 01:40:22 +0500

    new updated

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# ✅ Using SQLite (NO MySQL)
+# ✅ Using SQLite (NOMySQL)---------
 DATABASE_URL = "sqlite:///./cart.db"
 
 engine = create_engine(
